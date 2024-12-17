ebef84b2030bad576c21f28f9f48f547cd07a536
Nicolas <nicolastrujillloestevez@gmail.com>
2024-12-17 17:57:05 +0000

    esta con la logica del indexer para que no se pisen, la del crawler y la del query engine

--- crawler/src/main/java/software/cheeselooker/apps/Main.java
+++ crawler/src/main/java/software/cheeselooker/apps/Main.java
@@ -7,12 +7,14 @@ import software.cheeselooker.implementations.StoreInDatalake;
 import software.cheeselooker.ports.ReaderFromWebInterface;
 import software.cheeselooker.ports.StoreInDatalakeInterface;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.Files;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
 
 public class Main {
     public static void main(String[] args) {
@@ -29,9 +31,35 @@ public class Main {
 
     private static void periodicTask(ScheduledExecutorService scheduler, Command crawlerCommand) {
         scheduler.scheduleAtFixedRate(() -> {
+            File confirmationFile = new File(System.getProperty("user.dir") + "/data/confirmation");
+
+            if (confirmationFile.exists()) {
+                try {
+                    String content = new String(Files.readAllBytes(confirmationFile.toPath())).trim();
+                    if (content.equals("ok")) {
+                        System.out.println("Task already executed. Skipping this time.");
+                        return;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // Crear el archivo de confirmación con "ok"
+            try {
+                Files.write(confirmationFile.toPath(), "ok".getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             System.out.println("Starting download process...");
             crawlerCommand.download(3);
+
+            // Borrar el archivo de confirmación después de ejecutar la tarea
+            if (confirmationFile.exists()) {
+                confirmationFile.delete();
+            }
+
         }, 0, 20, TimeUnit.MINUTES);
     }
-
 }
